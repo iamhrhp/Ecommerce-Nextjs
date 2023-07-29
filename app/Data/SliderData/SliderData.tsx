eae0f776d417e4d86f8634327815a6aa5e8b427a
iamhrhp <hrhp007@gmail.com>
2023-07-30 05:07:34 +0530

    [hrhp] added slider carousle

--- app/Data/SliderData/SliderData.tsx
+++ app/Data/SliderData/SliderData.tsx
@@ -4,6 +4,12 @@ import summer from '../../Images/summer-appliance.jpg';
 import toys from '../../Images/toys.jpg';
 import bottle from '../../Images/water-bottle.jpg';
 
+export interface SliderDataProps {
+  id: number;
+  title: string;
+  img: string;
+}
+
 export const SliderData = [
   { id: 1, title: 'Shoe', img: shoe },
   { id: 2, title: 'mobile', img: mobile },
